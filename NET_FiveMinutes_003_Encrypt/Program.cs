d4cecccbc7cc0c51946240f6d507d4c62bf641f1
albertzhao <szdxzhy@outlook.com>
2022-08-17 08:37:53 +0800

    增加加密解密：MD5不可逆加密、DES对称可逆加密、RSA非对称可逆加密

---
+++ NET_FiveMinutes_003_Encrypt/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NET_FiveMinutes_003_Encrypt.Common;
+
+namespace NET_FiveMinutes_003_Encrypt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "telangpu";
+            Console.WriteLine($"原始字符串:{test}");
+            string encryptTestMd5 = Md5Encrypt.Encrypt(test);
+            Console.WriteLine($"Md5加密的字符串：{encryptTestMd5}");
+            string encryptTestDes = DesEncrypt.Encrypt(test);
+            Console.WriteLine($"Des加密的字符串：{encryptTestDes}");
+            Console.WriteLine($"Des还原的字符串：{DesEncrypt.Decrypt(encryptTestDes)}");
+            KeyValuePair<string, string> encryptDecrypt = RsaEncrypt.GetKeyPair();
+            string rsaEn1 = RsaEncrypt.Encrypt(test, encryptDecrypt.Key);
+            string rsaDe1 = RsaEncrypt.Decrypt(rsaEn1, encryptDecrypt.Value);
+            Console.WriteLine($"Rsa加密的字符串：{rsaEn1}");
+            Console.WriteLine($"Rsa还原的字符串：{rsaDe1}");
+            Console.ReadLine();
+        }
+    }
+
+    class Student
+    {
+        public string name;
+        public string age;
+    }
+}
